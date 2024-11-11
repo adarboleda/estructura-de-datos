7d6f72243811e564e2d0b3c926c35076aa2a6104
Abner Arboleda <93734334+adarboleda@users.noreply.github.com>
2024-11-11 16:13:00 -0500

    Recursividad no de cola

--- Deber1.2 (Tipos de Recursividad)/recursividadNoDeCola.cpp
+++ Deber1.2 (Tipos de Recursividad)/recursividadNoDeCola.cpp
@@ -7,3 +7,17 @@
  * Materia:                        Estructura de datos                 *
  * NRC :                           1992                                *
  ***********************************************************************/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int factorial(int n) {
+    if (n == 1) return 1;
+    return n * factorial(n - 1);  // La multiplicación se realiza después de la llamada recursiva
+}
+
+int main() {
+    cout << "Factorial de 5: " << factorial(5) << endl;  // Calcula 5 * 4 * 3 * 2 * 1 = 120
+    return 0;
+}
