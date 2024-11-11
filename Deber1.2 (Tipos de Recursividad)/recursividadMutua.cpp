6b281702906ff33822a1d9184e7b7fe4e5f7a93e
Abner Arboleda <93734334+adarboleda@users.noreply.github.com>
2024-11-11 16:18:26 -0500

    Recursividad mutua

--- Deber1.2 (Tipos de Recursividad)/recursividadMutua.cpp
+++ Deber1.2 (Tipos de Recursividad)/recursividadMutua.cpp
@@ -7,3 +7,31 @@
  * Materia:                        Estructura de datos                 *
  * NRC :                           1992                                *
  ***********************************************************************/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+void esPar(int n);
+void esImpar(int n);
+
+void esPar(int n) {
+    if (n == 0) {
+        cout << n << " es par" << endl;
+    } else {
+        esImpar(n - 1);  // Llama a esImpar
+    }
+}
+
+void esImpar(int n) {
+    if (n == 0) {
+        cout << n << " es impar" << endl;
+    } else {
+        esPar(n - 1);  // Llama a esPar
+    }
+}
+
+int main() {
+    esPar(4);  // Determina que 4 es par alternando llamadas entre esPar y esImpar
+    return 0;
+}
